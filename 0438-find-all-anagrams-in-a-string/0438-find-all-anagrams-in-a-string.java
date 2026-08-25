2b206f61d1fffe1c4f934a6e9b38ef95526d706f
Nagaraj Narendra Achari <acharinagaraj19@gmail.com>
2026-08-25 23:48:04 +0530

    Time: 1315 ms (5.37%), Space: 47.5 MB (23.28%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,33 +1,55 @@
 class Solution {
     StringBuilder countingSort(String s){
         char[] c=s.toCharArray();
-        int arr[]=new int[26];
+        int[] arr=new int[26];
         for(char ch:c){
             arr[ch-'a']++;
         }
         //retrieval
         StringBuilder sb=new StringBuilder();
         for(int i=0;i<26;i++){
+            int count=arr[i];
             while(arr[i]!=0){
                 sb.append((char) (i+'a'));
                 arr[i]--;
             }
+            arr[i]=count;
         }
         return sb;   
     } 
     public List<Integer> findAnagrams(String s, String p) { 
         List<Integer> list=new ArrayList<>(); 
+        int[] arr=new int[26];
         int i=0,j=p.length()-1;
-        StringBuilder sbs;
+        
         StringBuilder sbp=countingSort(p);
         p=sbp.toString();
+        if(p.length()<=s.length()){
+            for(int k=0;k<p.length();k++){
+                arr[s.charAt(k)-'a']++;
+            }
+        }
         while(j<s.length()){
-            sbs=countingSort(s.substring(i,j+1));
-            if(p.equals(sbs.toString())){
+            //retrieve
+            StringBuilder sbs=new StringBuilder();
+            for(int k=0;k<26;k++){
+                int c=arr[k];
+                while(arr[k]!=0){
+                    sbs.append((char) (k+'a'));
+                    arr[k]--;
+                }
+                arr[k]=c;
+            }
+            if(sbs.toString().equals(p)){
                 list.add(i);
             }
+            arr[s.charAt(i)-'a']--;
             i++;
             j++;
+
+            if(j<s.length()){
+                arr[s.charAt(j)-'a']++;
+            }
             
         }
         return list;
