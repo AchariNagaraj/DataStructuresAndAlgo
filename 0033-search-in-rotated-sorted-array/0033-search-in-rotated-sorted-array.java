d39d75b5faf33caf49941841a24bf6485461da80
Nagaraj Narendra Achari <acharinagaraj19@gmail.com>
2026-09-04 16:27:13 +0530

    Time: 0 ms (100%), Space: 43.7 MB (65.98%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -18,28 +18,27 @@ class Solution {
                         right=mid-1;
                     }
                 }
-                else{//nums[right]<nums[mid]
+                else{
                     left=mid+1;
                 }
             }
             else{//nums[mid]>target
-                if(nums[left]==target){
-                    return left;
-                }
                 if(nums[left]<nums[mid]){
-                    if(nums[left]<target){
-                        right=mid-1;
+                    if(nums[left]==target){
+                        return left;
                     }
-                    else{//nums[left]>target
+                    if(nums[left]>target){
                         left=mid+1;
                     }
+                    else{
+                        right=mid-1;
+                    }
                 }
-                else if(nums[left]>nums[mid]){//nums[left]>nums[mid]
-                    right=mid-1;
-
-                }
-                else{//left is same as mid
+                else if(nums[left]==nums[mid]){
                     left=mid+1;
+                }
+                else{
+                    right=mid-1;
                 }
             }
         }
